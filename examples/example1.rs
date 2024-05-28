904b86a5f22fd3feed145c611b6c59338eb609aa
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2024-05-28 18:10:30 +0200

    verify example1.rs, reorganize other examples

--- examples/example1.rs
+++ examples/example1.rs
@@ -1,3 +1,7 @@
 fn main() {
-    let _x = 42;
+    let mut x = 42;
+    x = x + 1;
+    assert!(x == 43);
+    let y = x * 2;
+    assert!(y == 86);
 }
--- examples/example3.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let mut x = 42;
-    {
-        // don't need `let mut y` as the reference stays the same
-        let y = &mut x;
-        *y = *y + 1;
-    }
-    assert!(x == 43);
-}
