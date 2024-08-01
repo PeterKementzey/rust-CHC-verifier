9e26da9607980a68e8a3f2e815eabeaa7e11babe
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2024-08-01 18:32:03 +0200

    Update readme and python script

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
-# How to
+# Rust Verifier
 
-Run verifier on example1.rs:
+A formal verifier for (a subset of) Rust programs using Constrained Horn Clauses. Uses prophecy variables to simplify the clauses for verifying pointers/references. Please read more in my master's thesis. <!-- TODO: add link -->
+
+## Prerequisites
+
+- [Rust](https://www.rust-lang.org/tools/install)
+- [Z3](https://github.com/Z3Prover/z3/releases)
+- Python 3 (tested with version 3.10.12)
+
+## How to
+
+Use the helper script to run the verifier on a Rust file:
 
 ```sh
-cargo run examples/example1.rs
+python3 verify.py examples/test1.rs
 ```
 
-Build all examples (don't forget to add new examples to `Cargo.toml`):
+Use the helper script to verify all files in the `examples` directory:
 
 ```sh
-cargo build --examples
+python3 verify.py
+```
+
+Run verifier on `examples/test1.rs`:
+
+```sh
+cargo run examples/test1.rs
 ```
 
 Run an example file (not analyze but execute):
 
 ```sh
-cargo run --example example1
+cargo run --example test1
 ```
--- verify.py
+++ verify.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 # Usage:
-# `python3 verify.py [optional list of args]`
-# Compiles project, creates smt2 file for each argument from `examples/example<arg>.rs` and runs z3 on
-# the created `examples/example<arg>.smt2`.
+# `python3 verify.py [optional list of rust files]`
+#
+# This scipt:
+#
+# 1. Compiles project
+# 2. Creates smt2 file with Constrained Horn Clauses from each argument
+# 3. Runs Z3 to check satisfiability of each smt2 file
+#
 # If no arguments are provided, it will run on all rust files in the examples directory.
 
 
@@ -17,29 +22,37 @@ def print_green(text):
     ENDC = '\033[0m'  # Reset to the default color
     print(f"{GREEN}{text}{ENDC}")
 
+def run_command(command: str):
+    print_green(f'Running: {command}')
+    subprocess.run(command, shell=True, check=True)
+
 
 def verify_example(example: str):
     source_file_name = example
     smt_file_name = example[:-3] + '.smt2'
-    command = f'./target/debug/rust-verifier examples/{source_file_name}'
-    print_green(f'Running: {command}')
-    subprocess.run(command, shell=True, check=True)
-    print_green(f'Created file {smt_file_name}:')
-    with open(f'examples/{smt_file_name}', 'r') as f:
+
+    print_green(f'Verifying {source_file_name}:')
+    with open(source_file_name, 'r') as f:
         print(f.read())
-    command = f'z3 examples/{smt_file_name}'
-    print_green(f'Running: {command}')
-    subprocess.run(command, shell=True, check=True)
+
+    run_command(f'./target/release/rust-verifier {source_file_name}')
+
+    print_green(f'Created file {smt_file_name}:')
+    with open(f'{smt_file_name}', 'r') as f:
+        print(f.read())
+
+    run_command(f'python3 pretty-print-assertions.py {smt_file_name}')
+
+    run_command(f'z3 {smt_file_name}')
 
 
 def main():
-    print_green('Running: cargo build')
-    subprocess.run('cargo build', shell=True, check=True)
+    run_command('cargo build --release')
 
     if len(sys.argv) < 2:
         for file in os.listdir('examples'):
             if file.endswith('.rs'):
-                verify_example(file)
+                verify_example(f'examples/{file}')
     else:
         for arg in sys.argv[1:]:
             verify_example(arg)
